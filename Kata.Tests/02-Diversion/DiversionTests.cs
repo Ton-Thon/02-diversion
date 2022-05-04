58b205c1a213f10eda9fbd92b9389e6da8ace21f
Ton-Thong <ton@wrmsoftware.com>
2022-05-04 23:32:39 +0700

    Red: Add test case failed

--- Kata.Tests/02-Diversion/DiversionTests.cs
+++ Kata.Tests/02-Diversion/DiversionTests.cs
@@ -14,6 +14,7 @@ namespace Kata.Tests._02_Diversion
             yield return new object[] { 1, new List<string> { "0", "1" } };
             yield return new object[] { 2, new List<string> { "00", "01", "10", "11" } };
             yield return new object[] { 3, new List<string> { "000", "001", "010", "011", "100", "101", "110", "111" } };
+            yield return new object[] { 4, new List<string> { "0000", "0001", "0010", "0011", "0100", "0101", "0110", "0111", "1000", "1001", "1010", "1011", "1100", "1101", "1110", "1111" } };
         }
 
         [Theory]
@@ -29,6 +30,5 @@ namespace Kata.Tests._02_Diversion
             // Assert
             act.Should().Equal(expected);
         }
-
     }
 }
