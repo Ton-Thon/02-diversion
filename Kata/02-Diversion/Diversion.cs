81577bc58b26bf4c7235665a88d6628c08dc82aa
Ton-Thong <ton@wrmsoftware.com>
2022-05-04 23:36:08 +0700

    change solution for solve GenerateBinaryListFromNumber

--- Kata/02-Diversion/Diversion.cs
+++ Kata/02-Diversion/Diversion.cs
@@ -9,18 +9,12 @@ namespace Kata._02_Diversion
     public class Diversion
     {
         //1. input 4
-        //2. 4 -> 1111
-        //3. 1111 -> 15
-        //4. generate loop i in 0 - 15
+        //2. 2 pow 4 -> 16
+        //4. generate loop i in 0 - 16
         //5. convert i -> binary
         public List<string> GenerateBinaryListFromNumber(int number)
         {
-            if (number == 2)
-                return new List<string> { "00", "01", "10", "11" };
-
-            if (number == 3)
-                return new List<string> { "000", "001", "010", "011", "100", "101", "110", "111" };
-
+            
 
             return new List<string> { "0", "1" };
         }
