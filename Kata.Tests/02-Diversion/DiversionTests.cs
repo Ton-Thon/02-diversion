9108ed46f6e1743f492ae81e36df890242e38fd4
Ton-Thong <ton@wrmsoftware.com>
2022-05-04 23:13:36 +0700

    Red: Add test case failed

---
+++ Kata.Tests/02-Diversion/DiversionTests.cs
@@ -0,0 +1,31 @@
+﻿using Kata._02_Diversion;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata.Tests._02_Diversion
+{
+    public class DiversionTests
+    {
+        public static IEnumerable<object[]> TestGenerateBinaryListFromNumberData()
+        {
+            yield return new object[] { 1, new List<string> { "0", "1" } };
+        }
+
+        [Theory]
+        [MemberData(nameof(TestGenerateBinaryListFromNumberData))]
+        public void GenerateBinaryListFromNumberTest(int number, List<string> expected)
+        {
+            // Arrange
+            var sut = new Diversion();
+
+            // Act
+            var act = sut.GenerateBinaryListFromNumber(number);
+
+            // Assert
+            act.Should().Equal(expected);
+        }
+    }
+}
---
+++ Kata/02-Diversion/Diversion.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata._02_Diversion
+{
+    public class Diversion
+    {
+        public List<string> GenerateBinaryListFromNumber(int number)
+        {
+            return new List<string> { number.ToString() };
+        }
+
+        public int GetDiversion(List<string> binaryList)
+        {
+            return binaryList.Count;
+        }
+    }
+}
